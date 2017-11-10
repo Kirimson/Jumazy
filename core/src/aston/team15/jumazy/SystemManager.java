c5edda94b22ec60a25fb8d3b8675dc4b9c2b04a8
Kirimson <kierangates@outlook.com>
2017-11-10 22:14:08 +0000

    Update documentation
    Added basic documentation to new classes. Will do more when I have time

--- core/src/aston/team15/jumazy/SystemManager.java
+++ core/src/aston/team15/jumazy/SystemManager.java
@@ -3,34 +3,51 @@ package aston.team15.jumazy;
 import java.util.Stack;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+/**
+ * Manages {@link MainSystem} instances, and is used directly with the main libGDX class. allows multiple systems to be added, while only the first system is ever accessed
+ * @author kieran, Jawwad
+ *
+ */
 public class SystemManager {
 
-	private Stack<MainSystem> currentSystem;
+	private Stack<MainSystem> systemStack;
 	
 	public SystemManager() {
-		currentSystem = new Stack<MainSystem>();
+		systemStack = new Stack<MainSystem>();
 	}
 	
 	private void pop() {
-		currentSystem.pop();
+		systemStack.pop();
 	}
 	
 	public void push(MainSystem system) {
-		currentSystem.push(system);
+		systemStack.push(system);
 	}
 	
+	/**
+	 * Removes the top system on the stack and replaces it with the new system to be used in game
+	 * @param system new {@link MainSystem} to be used
+	 */
 	public void setNewSystem(MainSystem system) {
 		pop();
 		push(system);
 	}
 
+	/**
+	 * Accesses the top system in the stack and calls its handleInput method
+	 * @return modified SpriteBatch to be drawn
+	 */
 	public void handleInput() {
-		currentSystem.peek().handleInput();
+		systemStack.peek().handleInput();
 	}
 	
+	/**
+	 * Accesses the top system in the stack and calls its draw method
+	 * @param batch SpriteBacth to be modified
+	 * @return modified SpriteBatch to be drawn
+	 */
 	public SpriteBatch draw(SpriteBatch batch) {
-		return currentSystem.peek().draw(batch);
+		return systemStack.peek().draw(batch);
 	}
 	
 }
