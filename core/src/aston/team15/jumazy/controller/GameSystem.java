247e24a74e0475a1df2f695df1234cb578a88530
Kirimson <kierangates@outlook.com>
2017-11-29 15:42:46 +0000

    davids fix

--- core/src/aston/team15/jumazy/controller/GameSystem.java
+++ core/src/aston/team15/jumazy/controller/GameSystem.java
@@ -51,6 +51,7 @@ public class GameSystem extends MainSystem{
 		&& maze.getCurrPlayer().rolled() == false 
 		&& maze.getCurrPlayer().isTrapped() == false) {
 			focusCamera();
+			maze.getCurrPlayer().setStartOfMove(maze.getCurrPlayer().getCoords());
 			maze.getCurrPlayer().switchRolled();
 			maze.getCurrPlayer().roll(maze.getWeather().getMovementMod());
 			System.out.println("Current player " + maze.getCurrPlayerVal());
@@ -59,7 +60,6 @@ public class GameSystem extends MainSystem{
 		if(Gdx.input.isKeyJustPressed(Input.Keys.ENTER) 
 		&& maze.getCurrPlayer().getRollSpaces() == 0
 		&& maze.getCurrPlayer().isTrapped() == false) {
-			maze.getCurrPlayer().switchRolled();
 			unfocusCamera();
 			maze.nextPlayer();
 			System.out.println("CHANGE TURN");
@@ -88,13 +88,6 @@ public class GameSystem extends MainSystem{
 			
 			if(direction != "")
 				maze.getCurrPlayer().newMove(direction);
-			
-//			if(maze.getCurrPlayer().getRollSpaces() == 0) {
-//				unfocusCamera();
-//				maze.nextPlayer();
-//				System.out.println("CHANGE TURN");
-//			}
-			
 		}
 		
 		if(maze.getCurrPlayer().isTrapped()) {
