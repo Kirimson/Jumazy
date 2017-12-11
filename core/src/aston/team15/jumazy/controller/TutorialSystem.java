34ac4b17b046184f0981d30368b894213f304672
Unknown <kierangates@outlook.com>
2017-12-11 23:04:54 +0000

    Added tutorial image

--- core/src/aston/team15/jumazy/controller/TutorialSystem.java
+++ core/src/aston/team15/jumazy/controller/TutorialSystem.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
+import aston.team15.jumazy.model.TextureConstants;
 import aston.team15.jumazy.view.JumazyGame;
 
 public class TutorialSystem extends MainSystem{
@@ -31,6 +33,18 @@ public class TutorialSystem extends MainSystem{
 		batch.draw(background, 0, 0, JumazyGame.WIDTH, JumazyGame.HEIGHT);
 		batch.draw(background, 0, 0, JumazyGame.WIDTH, JumazyGame.HEIGHT);
 		batch.draw(backButton4,(JumazyGame.WIDTH/2)-(backButton4.getWidth()/2)+500,(JumazyGame.HEIGHT/2)-(backButton4.getHeight()/2)-300,150,50);
+		
+		Texture tutTex = TextureConstants.getTexture("Monkey Menu");
+    	Sprite tutSprite = new Sprite(tutTex);
+    	
+    	
+    	tutSprite.setRegion(tutTex);
+    	tutSprite.setSize((tutTex.getWidth()*cam.zoom)/3, (tutTex.getHeight()*cam.zoom)/3);
+    	tutSprite.setX(cam.position.x-(tutTex.getWidth()*cam.zoom)/6);
+    	tutSprite.setY(cam.position.y-((tutTex.getHeight()*cam.zoom)/3-(JumazyGame.HEIGHT/2*cam.zoom)));
+    	
+    	tutSprite.draw(batch);
+		
 		
 	}
 
