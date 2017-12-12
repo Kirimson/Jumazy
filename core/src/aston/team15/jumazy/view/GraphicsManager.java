9887e4a8e8c5e16077cc55069e490863e8ab2ce3
Kirimson <kierangates@outlook.com>
2017-12-12 12:37:55 +0000

    whoops. forgot to import button

--- core/src/aston/team15/jumazy/view/GraphicsManager.java
+++ core/src/aston/team15/jumazy/view/GraphicsManager.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 
+import aston.team15.jumazy.controller.Button;
 import aston.team15.jumazy.model.DieAnimation;
 import aston.team15.jumazy.model.Maze;
 import aston.team15.jumazy.model.Player;
