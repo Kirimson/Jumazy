cc75de1e40bf0a8bbb154a4909c6cc011a6b44c4
Unknown <kierangates@outlook.com>
2018-02-19 00:00:29 +0000

    Way better room generation
    Now the params are: room amount and room size.
    the algo then makes the amount of rooms long a decent amount, and sets up maze array no matter the arguments

--- core/src/aston/team15/jumazy/model/Generator.java
+++ core/src/aston/team15/jumazy/model/Generator.java
@@ -1,7 +1,5 @@
 package aston.team15.jumazy.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 
 public class Generator {
@@ -17,40 +15,50 @@ public class Generator {
 
 	public static void main(String[] args) {
 		Generator gen = new Generator();
-		gen.genMaze(30,30);
+		gen.genMaze(10,11);
 	}
 
 	/**
 	 * Creates a maze based on rooms connected to each other
-	 * @param dimensionx x dimension of maze
-	 * @param dimensiony y dimension of maze
+	 * @param roomAmount amount of rooms in maze
+	 * @param roomSize the size of each room (currently just a square)
 	 * @return the maze
 	 */
-	public String[][] genMaze(int dimensionx, int dimensiony){
+	public String[][] genMaze(int roomAmount, int roomSize){
 
 		//currently has a hard set size for each room (10*10)
-		int roomsize = 10;
+		int roomsAcross = 3;
+		for(int i = 1; i <= 10; i++){
+			if(roomAmount % i == 0 && i !=roomAmount)
+				roomsAcross = i;
+		}
+		System.out.println(roomsAcross);
+		//loop from 1-10, mod roomAmount with it, first to produce 0 is roomsAcross. simples
 
 		//amount of rooms that will be across and up the maze
-		int roomlength = dimensionx/roomsize; //3
+
+
+		int extraRoom = (roomAmount % roomsAcross == 0 ? 0 : 1);
+
+		int roomsDown = roomAmount / roomsAcross + extraRoom;
 
 		//total amount of rooms in maze
-		int roomAmount = roomlength * roomlength; //9
+//		int roomAmount = roomlength * roomlength; //9
 
 		//create maze with enough space to store all cells of all rooms in a square shape
-		maze = new String[roomsize*roomlength][roomsize*roomlength];
+		maze = new String[roomSize*roomsDown][roomSize*roomsAcross];
 
 		//create roomAmount rooms
 		for (int i = 0; i < roomAmount; i++) {
-			String[][] room = genRoom(roomsize);
+			String[][] room = genRoom(roomSize);
 
 			//set the xoffset to start putting cells into the maze
 			// room 0 will have offset 0, room 3 will also have offset 0 in this example (a 30*30 maze)
-			int xoffset=(i*roomsize % (roomsize*roomlength)); //xoffset
+			int xoffset=(i*roomSize % (roomSize*roomsDown)); //xoffset
 
 			//y offset for placing cells of room into maze
 			//when over the limit of the length, move cells down
-			int yoffset=(i*roomsize / (roomsize*roomlength)*roomsize); //xoffset
+			int yoffset=(i*roomSize / (roomSize*roomsDown)*roomSize); //xoffset
 
 			//add cells into maze using offsets
 			for(int mazerow = 0; mazerow < room.length; mazerow++){
@@ -64,7 +72,9 @@ public class Generator {
 		//prints out rooms
 		for(int mazerow = 0; mazerow < maze.length; mazerow++){
 			for (int mazecol = 0; mazecol < maze[0].length; mazecol++){
-				System.out.print(maze[mazerow][mazecol]);
+				String current = maze[mazerow][mazecol];
+				if(current != null)
+					System.out.print(current);
 			}
 			System.out.println();
 		}
@@ -81,9 +91,9 @@ public class Generator {
 
 		String[][] room = new String[size][size];
 
-		for(int i = 0; i <= 9; i++){
-			for (int k = 0; k <= 9; k++){
-				if(i == 0 || i == 9 || k == 0 || k == 9){
+		for(int i = 0; i < size; i++){
+			for (int k = 0; k < size; k++){
+				if(i == 0 || i == size-1 || k == 0 || k == size-1){
 					room[i][k] = "*";
 				}
 				else{
