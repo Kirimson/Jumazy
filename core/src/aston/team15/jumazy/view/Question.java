55b1d2b17bc54c6c49cacacb8469fa8dd7af5149
mohams11 <smohammadi1570@gmail.com>
2018-03-04 14:59:59 +0000

    Questions class refactor
    Seperated retrieving from the csv files from the questions UI class.

--- core/src/aston/team15/jumazy/view/Question.java
+++ core/src/aston/team15/jumazy/view/Question.java
@@ -1,13 +1,5 @@
 package aston.team15.jumazy.view;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -20,29 +12,25 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
+import aston.team15.jumazy.controller.QuestionsRetriever;
+
 public class Question extends ApplicationAdapter{
 
 	
 	private Stage stage;
 	private Skin skin;
-	private String[] cells = null;
 	private boolean isActive;
 	private boolean correct;
-	
-	private boolean geo = true;
-	private String geoLevel = "Medium";
-	private boolean maths = true;
-	private String mathsLevel = "Easy";
+	private QuestionsRetriever question = new QuestionsRetriever();
 	
 	
 	public void create () {
-		
 		isActive = true;
 		stage = new Stage(new ScreenViewport());
 		skin = new Skin(Gdx.files.internal("../core/assets/neon/skin/neon-ui.json"));
 		TextButton btnSubmit = new TextButton("submit", skin);
 		TextField tfAnswer = new TextField("", skin);
-		String[] riddleText = retrieveRiddle();
+		String[] riddleText = question.retrieveRiddle();
 		Label lQuestion = new Label(riddleText[0], skin);
 		
 		lQuestion.setPosition(350, 450);
@@ -58,72 +46,14 @@ public class Question extends ApplicationAdapter{
 		
 		btnSubmit.addListener(new ClickListener() {
 			public void clicked(InputEvent event, float x, float y) {
-				correct = checkAnswer(tfAnswer.getText());
+				correct = question.checkAnswer(tfAnswer.getText());
 				isActive = false;
-				
-				if (correct == true) {
-					System.out.println("Yeaaaaah bitchh");
-				} 
-				else {
-					System.out.println("Nooooooo bitchh");
-				}
 			}
 		});
 		
 	}
 
 
-	public String selectFile() {
-		Random rand = new Random();
-		int  n = rand.nextInt(2) + 1;
-		String fileName = "";
-		
-		if(geo == true && n == 1) {
-			fileName = "../core/assets/questions/geography" + geoLevel + ".csv";
-		}
-		else if(maths == true && n == 2) {
-			fileName =  "../core/assets/questions/maths" + mathsLevel + ".csv";
-		}
-		
-		return fileName;
-	}
-	
-	public String[] retrieveRiddle() {
-		String fileName = selectFile();
-		File file = new File(fileName);
-
-		try {
-			Scanner inputStream = new Scanner(file);
-			String line;
-			List<String> lines = new ArrayList<>();
-			while (inputStream.hasNext()) {
-				line = inputStream.nextLine();
-				lines.add(line);
-
-			}
-
-			Collections.shuffle(lines);
-
-			cells = lines.get(0).split(",");
-		} catch (FileNotFoundException e) {
-			System.out.println(e);
-		}
-
-		return cells;
-	}
-
-	public boolean checkAnswer(String answer) {
-		int i = 1;
-		while (i < cells.length) {
-			if (answer.toLowerCase().equals(cells[i].toLowerCase())) {
-				System.out.println("correct");
-				return true;
-			}
-			i++;
-		}
-		return false;
-	}
-	
 	public boolean isAlive() {
 		return isActive;
 	}
