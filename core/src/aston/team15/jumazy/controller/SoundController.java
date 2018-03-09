bf47ad4d930943207149aa22c8c7825e2744554a
Unknown <domdom2609@gmail.com>
2018-03-09 20:56:13 +0000

    sound class added music in assests

---
+++ core/src/aston/team15/jumazy/controller/SoundController.java
@@ -0,0 +1,12 @@
+package aston.team15.jumazy.controller;
+
+import com.badlogic.gdx.audio.Sound;
+
+public class SoundController {
+
+	private Sound menuMusic;
+	
+	public SoundController() {
+		
+	}
+}
