57e53648e8412aa308b1a8edb00d5c52573a8cf9
mohams11 <smohammadi1570@gmail.com>
2018-03-12 12:36:23 +0000

    code cleanup

--- core/src/aston/team15/jumazy/view/QuestionPopUpCreator.java
+++ core/src/aston/team15/jumazy/view/QuestionPopUpCreator.java
@@ -18,73 +18,36 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class QuestionPopUpCreator {
 	
-	Skin skin;
-	Table questionTable;
-	Table questionBG;
-	CheckBox maths;
-	CheckBox geography;
-	CheckBox history;
-	String[] geoObjects;
-	String[] mathObjects;
-	String[] histoObjects;
-	SelectBox<String> geoSB;
-	SelectBox<String> mathSB;
-	SelectBox<String> histoSB;
-	HashMap<String, String> selections;
+	private Skin skin;
+	private Table questionTable;
+	private Table questionBG;
+	private CheckBox maths;
+	private CheckBox geography;
+	private CheckBox history;
+	private String[] geoObjects;
+	private String[] mathObjects;
+	private String[] histoObjects;
+	private SelectBox<String> geoSB;
+	private SelectBox<String> mathSB;
+	private SelectBox<String> histoSB;
+	private HashMap<String, String> selections;
 	
 	public QuestionPopUpCreator() {
 		skin = new Skin(Gdx.files.internal("jumazyskin/jumazy-skin.json"));
 		
-		questionTable = new Table();
-		questionTable.center();
-		questionTable.setFillParent(true);
-		
-		questionBG = new Table();
-		questionBG.setFillParent(true);
-		questionBG.top().padTop(0f);
 
+		createCheckBoxes();
+
+	    createSelectBoxes();
 		
-	    geography = new CheckBox("Geography", skin);
-	    maths = new CheckBox("Maths", skin);
-	    history = new CheckBox("History", skin);
-	    
-	    geoObjects = new String[4]; 
-	    geoObjects[0] = new String("Tick Geography for levels"); 
-	    geoObjects[1] = new String("Easy"); 
-	    geoObjects[2] = new String("Medium"); 
-	    geoObjects[3] = new String("Hard"); 
-	    
-	    mathObjects = new String[4]; 
-	    mathObjects[0] = new String("Tick Maths for levels"); 
-	    mathObjects[1] = new String("Easy"); 
-	    mathObjects[2] = new String("Medium"); 
-	    mathObjects[3] = new String("Hard"); 
-	    
-	    histoObjects = new String[4]; 
-	    histoObjects[0] = new String("Tick History for levels"); 
-	    histoObjects[1] = new String("Easy"); 
-	    histoObjects[2] = new String("Medium"); 
-	    histoObjects[3] = new String("Hard"); 
-	   
-	    geoSB = new SelectBox<String>(skin);
-		mathSB = new SelectBox<String>(skin);
-		histoSB = new SelectBox<String>(skin);
+	    createTables();
 		
-		geoSB.setItems(geoObjects);
-		mathSB.setItems(mathObjects);
-		histoSB.setItems(histoObjects);
-		
-		mathSB.setTouchable(Touchable.disabled);
-		geoSB.setTouchable(Touchable.disabled);
-		histoSB.setTouchable(Touchable.disabled);
-		
-		questionTable.add(geography).padTop(-20).left().row();;
-		questionTable.add(maths).left().row();;
-		questionTable.add(history).left().row();
-		
-		questionTable.add(geoSB).padTop(10).width(320).height(40).row();
-		questionTable.add(mathSB).width(320).height(40).row();
-		questionTable.add(histoSB).width(320).height(40).row();
+
+
+	}
+	
+	public void listeners() {
+
 		
 		maths.addListener(new ClickListener() {
 			public void clicked(InputEvent event, float x, float y) {
@@ -133,7 +96,62 @@ public class QuestionPopUpCreator {
 				
 			}
 		});
-
+	}
+	
+	public void createTables() {
+		questionTable = new Table();
+		questionTable.center();
+		questionTable.setFillParent(true);
+		
+		questionBG = new Table();
+		questionBG.setFillParent(true);
+		questionBG.top().padTop(0f);
+		
+		questionTable.add(geography).padTop(-20).left().row();;
+		questionTable.add(maths).left().row();;
+		questionTable.add(history).left().row();
+		
+		questionTable.add(geoSB).padTop(10).width(320).height(40).row();
+		questionTable.add(mathSB).width(320).height(40).row();
+		questionTable.add(histoSB).width(320).height(40).row();
+	}
+	
+	public void createCheckBoxes() {
+		geography = new CheckBox("Geography", skin);
+	    maths = new CheckBox("Maths", skin);
+	    history = new CheckBox("History", skin);
+	}
+	
+	public void createSelectBoxes() {
+	    geoObjects = new String[4]; 
+	    geoObjects[0] = new String("Tick Geography for levels"); 
+	    geoObjects[1] = new String("Easy"); 
+	    geoObjects[2] = new String("Medium"); 
+	    geoObjects[3] = new String("Hard"); 
+	    
+	    mathObjects = new String[4]; 
+	    mathObjects[0] = new String("Tick Maths for levels"); 
+	    mathObjects[1] = new String("Easy"); 
+	    mathObjects[2] = new String("Medium"); 
+	    mathObjects[3] = new String("Hard"); 
+	    
+	    histoObjects = new String[4]; 
+	    histoObjects[0] = new String("Tick History for levels"); 
+	    histoObjects[1] = new String("Easy"); 
+	    histoObjects[2] = new String("Medium"); 
+	    histoObjects[3] = new String("Hard"); 
+	   
+	    geoSB = new SelectBox<String>(skin);
+		mathSB = new SelectBox<String>(skin);
+		histoSB = new SelectBox<String>(skin);
+		
+		geoSB.setItems(geoObjects);
+		mathSB.setItems(mathObjects);
+		histoSB.setItems(histoObjects);
+		
+		mathSB.setTouchable(Touchable.disabled);
+		geoSB.setTouchable(Touchable.disabled);
+		histoSB.setTouchable(Touchable.disabled);
 	}
 	
 	public void populateSelections() {
