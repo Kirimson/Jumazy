c4c3e279fdf6ce3b2c04e0a1dc9f30ddca83f9b5
Unknown <4b5huja@gmail.com>
2018-03-13 21:32:56 +0000

    Removed unnecessary method from Item enum

--- core/src/aston/team15/jumazy/model/Item.java
+++ core/src/aston/team15/jumazy/model/Item.java
@@ -4,18 +4,12 @@ public enum Item {
 	RED_POTION(4, "hp"), BLUE_POTION(2, "stamina"), GREEN_POTION(2, "agility"), SWORD(2, "strength");
 
 	private final int value;
-	private final String stat;
 
 	private Item(int value, String stat) {
 		this.value = value;
-		this.stat = stat;
 	}
 
 	public int getValue() {
 		return value;
-	}
-	
-	public String getStat() {
-		return stat;
 	}
 }
