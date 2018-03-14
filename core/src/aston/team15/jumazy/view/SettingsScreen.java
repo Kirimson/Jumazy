6a1ccfeb8019aa48dd670e15db0dda443075bd99
Unknown <domdom2609@gmail.com>
2018-03-14 13:41:39 +0000

    kieran stuff

--- core/src/aston/team15/jumazy/view/SettingsScreen.java
+++ core/src/aston/team15/jumazy/view/SettingsScreen.java
@@ -1,34 +1,43 @@
 package aston.team15.jumazy.view;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import aston.team15.jumazy.controller.JumazyController;
 
 public class SettingsScreen extends MenuScreen {
 	
-	private String sliderVal;
+	private Label percentage;
+	private Slider volumeSlider;
 	
 	public SettingsScreen(JumazyController theGame) {
 		super(theGame);
 
 		MenuScreenButton backButton = new MenuScreenButton("BACK", MenuScreens.MAIN_MENU_SCREEN, game);
 		Skin skin = new Skin(Gdx.files.internal("jumazyskin/jumazy-skin.json"));
-		Slider volumeSlider = new Slider(0, 10, 1, false, skin);
-		Label percentage = new Label(sliderVal, skin);
+		volumeSlider = new Slider(0, 10, 1, false, skin);
+		String sliderVal = "" + volumeSlider.getValue();
+		percentage = new Label(sliderVal, skin);
 		
 		table.debug();
 		table.add(volumeSlider);
-		table.add(sliderVal);
+		table.row();
+		table.add(percentage);
 		table.row();
 		table.add(backButton).bottom().right().expand().pad(70);
-
+		
 		stage.addActor(table);
 	}
 
+	public void render() {
+		percentage.setText("" + volumeSlider.getValue());
+	}
+	
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
