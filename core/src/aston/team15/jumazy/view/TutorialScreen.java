8ab6b4dfb0c2b48b77c4a5b56b935372b553a333
mohams11 <smohammadi1570@gmail.com>
2018-03-14 11:38:12 +0000

    background for tutorial

--- core/src/aston/team15/jumazy/view/TutorialScreen.java
+++ core/src/aston/team15/jumazy/view/TutorialScreen.java
@@ -1,5 +1,8 @@
 package aston.team15.jumazy.view;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
 import aston.team15.jumazy.controller.JumazyController;
 
 public class TutorialScreen extends MenuScreen {
@@ -8,14 +11,16 @@ public class TutorialScreen extends MenuScreen {
 		super(theGame);
 
 		MenuScreenButton backButton = new MenuScreenButton("Back", MenuScreens.START_GAME_SCREEN, game);
-
+		Table tutorialBG = new Table();
+		tutorialBG.setFillParent(true);
+		tutorialBG.top().padTop(-30);
+		tutorialBG.add(new Image(game.getSprite("pause-dialog")));
 		table.row();
 		table.add(backButton).bottom().right().expand().pad(70);
-
+		stage.addActor(tutorialBG);
 		stage.addActor(table);
-
 	}
-
+	
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
