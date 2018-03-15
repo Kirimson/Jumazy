56f66531e83e05fd965062222dade8a46dd13d6c
mohams11 <smohammadi1570@gmail.com>
2018-03-15 16:36:56 +0000

    tutorial screen done

--- core/src/aston/team15/jumazy/view/TutorialScreen.java
+++ core/src/aston/team15/jumazy/view/TutorialScreen.java
@@ -2,121 +2,173 @@ package aston.team15.jumazy.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import aston.team15.jumazy.controller.JumazyController;
 
 public class TutorialScreen extends MenuScreen {
 	
-
+	private Skin skin;
+	private Table controlTable;
+	private Table itemsTable;
+	private Table statsTable;
+	private Table tutorialBG;
+	private Label description;
 	
 	public TutorialScreen(JumazyController theGame) {
 		super(theGame);
-		Skin skin = new Skin(Gdx.files.internal("jumazyskin/jumazy-skin.json"));
+		
+		skin = new Skin(Gdx.files.internal("jumazyskin/jumazy-skin.json"));
+		
+		background();
+		controlsSection();
+		itemsSection();
+		statsSection();
 		
 		MenuScreenButton backButton = new MenuScreenButton("Back", MenuScreens.START_GAME_SCREEN, game);
+		table.add(backButton).bottom().right().expand().pad(70);
+		stage.addActor(table);
+	}
+	
+	public void background() {
 		
-		Table tutorialBG = new Table();
+		tutorialBG = new Table();
 		tutorialBG.setFillParent(true);
 		tutorialBG.center();
-		tutorialBG.add(new Image(game.getSprite("pause-dialog"))).height(1200).width(1400).padTop(-300);
+		tutorialBG.add(new Image(game.getSprite("pause-dialog"))).height(1200).width(1100).padTop(-300);
+		stage.addActor(tutorialBG);
+	}
+	
+	public void controlsSection() {
 		
-		Table controlTable = new Table();
+		controlTable = new Table();
 		controlTable.setFillParent(true);
-		controlTable.top().padTop(50).left().padLeft(50);
+		controlTable.top().padTop(50).left().padLeft(180);
 		
 		Image enterKey = new Image(new Texture("enter.png"));
 		controlTable.add(enterKey).width(40).height(60);
-		Label enterDescription = new Label("Press Enter to switch players.", skin);
-		enterDescription.setFontScale(0.75f);
-		controlTable.add(enterDescription).padLeft(20);
+		description = new Label("Press enter to switch players.", skin);
+		description.setFontScale(0.75f);
+		controlTable.add(description).padLeft(40);
 		controlTable.row();
 		
 		Image spaceKey = new Image(new Texture("space.png"));
-		controlTable.add(spaceKey).width(50).height(20).padTop(20);
-		Label spaceDescription = new Label("Press space to roll the dice.", skin);
-		spaceDescription.setFontScale(0.75f);
-		controlTable.add(spaceDescription).padLeft(20).padTop(20);
+		controlTable.add(spaceKey).width(90).height(20).padTop(20);
+		description = new Label("Press space to roll the dice.", skin);
+		description.setFontScale(0.75f);
+		controlTable.add(description).padLeft(30).padTop(20);
 		controlTable.row();
 		
 		Image leftKey = new Image(new Texture("left.png"));
 		Image upKey = new Image(new Texture("up.png"));
 		Image downKey = new Image(new Texture("down.png"));
 		Image rightKey = new Image(new Texture("right.png"));
-		controlTable.add(leftKey).padTop(60).size(30).padLeft(-80);
-		controlTable.add(downKey).padTop(60).padLeft(-300).size(30);
-		controlTable.add(upKey).size(30).padLeft(-538).padTop(-5);
-		controlTable.add(rightKey).size(30).padLeft(-470).padTop(60);
-		Label arrowKeysDescription = new Label("Use the arrow keys to move your character.", skin);
-		arrowKeysDescription.setFontScale(0.75f);
-		controlTable.add(arrowKeysDescription).padTop(40).padLeft(-200);
+		controlTable.add(leftKey).padTop(60).size(30).padLeft(-60);
+		controlTable.add(downKey).padTop(60).padLeft(-343).size(30);
+		controlTable.add(upKey).size(30).padLeft(-600).padTop(-5);
+		controlTable.add(rightKey).size(30).padLeft(-535).padTop(60);
+		description = new Label("Use the arrow keys to move your character.", skin);
+		description.setFontScale(0.75f);
+		controlTable.add(description).padTop(40).padLeft(-215);
 		
-		//Stats
-		Table statsTable = new Table();
-		statsTable.setFillParent(true);
-		statsTable.bottom().padBottom(200).left().padLeft(50);
+		stage.addActor(controlTable);
+	}
+	
+	public void itemsSection() {
 		
-		//Items
-		Table itemsTable = new Table();
+		itemsTable = new Table();
 		itemsTable.setFillParent(true);
-		itemsTable.bottom().padBottom(200).left().padLeft(50);
+		itemsTable.right().padRight(200).top().padTop(50);
 		
-		Label itemsTitle = new Label("Items", skin);
-		itemsTitle.setFontScale(1.1f);
-		itemsTable.add(itemsTitle);
+		description = new Label("Items", skin);
+		description.setFontScale(1.1f);
+		itemsTable.add(description);
 		itemsTable.row();
 		
 		Image healthPotion = new Image(game.getSprite("potion-red"));
-		Label healthPotionDescription = new Label("+4 Health", skin);
-		healthPotionDescription.setFontScale(0.75f);
+		description = new Label("+4 Health", skin);
+		description.setFontScale(0.75f);
 		itemsTable.add(healthPotion).size(40);
-		itemsTable.add(healthPotionDescription);
+		itemsTable.add(description);
 		Image staminaPotion = new Image(game.getSprite("potion-blue"));
-		Label staminaPotionDescription = new Label("+2 Stamina", skin);
-		staminaPotionDescription.setFontScale(0.75f);
+		description = new Label("+2 Stamina", skin);
+		description.setFontScale(0.75f);
 		itemsTable.add(staminaPotion).size(40).padLeft(20);
-		itemsTable.add(staminaPotionDescription);
+		itemsTable.add(description);
 		Image luckPotion = new Image(game.getSprite("potion-green"));
-		Label luckPotionDescription = new Label("+2 Luck", skin);
-		luckPotionDescription.setFontScale(0.75f);
+		description = new Label("+2 Luck", skin);
+		description.setFontScale(0.75f);
 		itemsTable.add(luckPotion).size(40).padLeft(20);
-		itemsTable.add(luckPotionDescription);
+		itemsTable.add(description);
 		itemsTable.row();
 		
 		Image sword = new Image(game.getSprite("sword"));
-		Label swordDescription = new Label("+2 Strength", skin);
-		swordDescription.setFontScale(0.75f);
+		description = new Label("+2 Strength", skin);
+		description.setFontScale(0.75f);
 		itemsTable.add(sword).size(40).padTop(10);
-		itemsTable.add(swordDescription).padTop(10);
+		itemsTable.add(description).padTop(10).padLeft(10);
 		Image bow = new Image(game.getSprite("arrow"));
-		Label bowDescription = new Label("+1 strength", skin);
-		bowDescription.setFontScale(0.75f);
+		description = new Label("+1 strength", skin);
+		description.setFontScale(0.75f);
 		itemsTable.add(bow).size(40).padLeft(20).padTop(10);
-		itemsTable.add(bowDescription).padTop(10);
+		itemsTable.add(description).padTop(10);
 		itemsTable.row();
 		
 		
 		Image apple = new Image(game.getSprite("apple"));
-		Label appleDescription = new Label("+2 Health", skin);
-		appleDescription.setFontScale(0.75f);
-		itemsTable.add(apple).size(30).padTop(10);
-		itemsTable.add(appleDescription).padTop(10);
+		description = new Label("+2 Health", skin);
+		description.setFontScale(0.75f);
+		itemsTable.add(apple).size(30).padTop(10).padLeft(-10);
+		itemsTable.add(description).padTop(10);
 		Image grapes = new Image(game.getSprite("grapes"));
-		Label grapesDescription = new Label("+2 Health", skin);
-		grapesDescription.setFontScale(0.75f);
+		description = new Label("+2 Health", skin);
+		description.setFontScale(0.75f);
 		itemsTable.add(grapes).size(30).padTop(10);
-		itemsTable.add(grapesDescription).padTop(10);
+		itemsTable.add(description).padTop(10);
 		
-		
-		table.row();
-		stage.addActor(tutorialBG);
-		table.add(backButton).bottom().right().expand().pad(70);
-		stage.addActor(controlTable);
 		stage.addActor(itemsTable);
-		stage.addActor(table);
+	}
+	
+	public void statsSection() {
+		
+		statsTable = new Table();
+		statsTable.setFillParent(true);
+		statsTable.center().padTop(150);
+		
+		description = new Label("Stats & Gameplay", skin);
+		description.setFontScale(1.1f);
+		statsTable.add(description).padLeft(-450);
+		statsTable.row();
+		
+		description = new Label("HP: This is your health and if it hits 0 you will be spawned back where you started. "
+				+ "\nSTAMINA: The amount of stamina you have is added onto your dice roll."
+				+ "\nLUCK: This effects your chances of recieving an item from a chest."
+				+ "\nSTRENGTH: This increases your chances of winning fights against monsters.", skin);
+		description.setFontScale(0.75f);
+		statsTable.add(description);
+		statsTable.row();
+		
+		Image chest = new Image(new Texture("chest-gold.png"));
+		statsTable.add(chest).size(50).padLeft(-600);
+		description = new Label("You can recieve special items to help you towards vitory", skin);
+		description.setFontScale(0.75f);
+		statsTable.add(description).padLeft(-740);
+		statsTable.row();
+		
+		Image trap = new Image(game.getSprite("floor-trap-spikes"));
+		statsTable.add(trap).size(30).padLeft(-600);
+		description = new Label("These are traps! if stepped on a question will have to be answered."
+				+ "\nGet it correct, you may pass. Get it wrong and you will be punished.", skin);
+		description.setFontScale(0.75f);
+		statsTable.add(description).padLeft(-650);
+		
+		stage.addActor(statsTable);
 	}
 	
 	@Override
