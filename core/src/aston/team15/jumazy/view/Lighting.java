22bd1ca717a0db4a653a9eb5695cc03c6e3acc81
Kirimson <kierangates@outlook.com>
2018-03-16 23:13:41 +0000

    Lighting now pulsates
    goes between 1x scale and 1.2x scale, over what feels like 2 seconds. just using more delta time to determine scale, just divided by 10 to make it slower

--- core/src/aston/team15/jumazy/view/Lighting.java
+++ core/src/aston/team15/jumazy/view/Lighting.java
@@ -1,5 +1,6 @@
 package aston.team15.jumazy.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
@@ -9,14 +10,31 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 public class Lighting extends Image{
 
     private Sprite lightImage;
+    private float timeScale = 1f;
+    boolean increaseSize = true;
 
     public Lighting() {
         lightImage = new Sprite(new Texture("light.png"));
-
+        lightImage.setOriginCenter();
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha){
+
+        if(increaseSize){
+            timeScale += Gdx.graphics.getRawDeltaTime()/10;
+            if(timeScale > 1.2f)
+                increaseSize = false;
+        }
+
+        if(!increaseSize){
+            timeScale -= Gdx.graphics.getRawDeltaTime()/10;
+            if(timeScale < 1.01f)
+                increaseSize = true;
+        }
+
+        lightImage.setScale(timeScale);
+
         lightImage.draw(batch);
     }
 
