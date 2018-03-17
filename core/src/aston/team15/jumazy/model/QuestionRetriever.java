5e914ff6181cb4c5a04bf0a3c64d5bda386cb574
Kirimson <kierangates@outlook.com>
2018-03-17 04:50:28 +0000

    QuestionReterivier and QuestionUI to libGDX
    a lot simpler than the room parser thankfully. set the scanner to import the inputbuffer returned by FileHandle.read() and made sound clips play through GameSound and a new Sound var

--- core/src/aston/team15/jumazy/model/QuestionRetriever.java
+++ core/src/aston/team15/jumazy/model/QuestionRetriever.java
@@ -1,5 +1,8 @@
 package aston.team15.jumazy.model;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -41,7 +44,7 @@ public class QuestionRetriever {
 		String selectedType = questionRandomiser.get(0);
 
 		//return the URI of the file. has no checks if the file inst there, as they should soon be put in
-		return "../assets/questions/"+selectedType.toLowerCase()+categoryLevels.get(selectedType)+".csv";
+		return "questions/"+selectedType.toLowerCase()+categoryLevels.get(selectedType)+".csv";
 	}
 
 	/**
@@ -50,23 +53,20 @@ public class QuestionRetriever {
 	 */
 	public String[] retrieveRiddle() {
 		String fileName = selectFile();
-		File file = new File(fileName);
+
+		FileHandle csv = Gdx.files.internal(fileName);
 
 		do {
-			try {
-				Scanner inputStream = new Scanner(file);
-				String line;
-				List<String> lines = new ArrayList<>();
-				while (inputStream.hasNext()) {
-					line = inputStream.nextLine();
-					lines.add(line);
-				}
-
-				Collections.shuffle(lines);
-				cells = lines.get(0).split("_");
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+			Scanner inputStream = new Scanner(csv.read());
+			String line;
+			List<String> lines = new ArrayList<>();
+			while (inputStream.hasNext()) {
+				line = inputStream.nextLine();
+				lines.add(line);
 			}
+
+			Collections.shuffle(lines);
+			cells = lines.get(0).split("_");
 		}while (cells[0].equals(lastQuestion));
 
 		lastQuestion = cells[0];
