84ba5cbbdfa053c2ac62140be589d8595368d238
Unknown <domdom2609@gmail.com>
2018-03-17 03:06:39 +0000

    Dreamer texture menu

--- core/src/aston/team15/jumazy/view/TextureSelectionScreen.java
+++ core/src/aston/team15/jumazy/view/TextureSelectionScreen.java
@@ -1,5 +1,9 @@
 package aston.team15.jumazy.view;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
 import aston.team15.jumazy.controller.JumazyController;
 
 public class TextureSelectionScreen extends MenuScreen {
@@ -12,15 +16,22 @@ public class TextureSelectionScreen extends MenuScreen {
 		MenuScreenButton texture3 = new MenuScreenButton("Texture 3", null, game);
 		MenuScreenButton backButton = new MenuScreenButton("Back", MenuScreens.SETTINGS_SCREEN, game);
 
-		table.add(texture1).pad(10);
-		table.row();
-		table.add(texture2).pad(10);
-		table.row();
-		table.add(texture3).pad(10);
-		table.row();
+		Table textureTable = new Table();
+
+		textureTable.setFillParent(true);
+		textureTable.setPosition(0.0f, -150.0f);
+		textureTable.row();
+		textureTable.add(texture1).pad(100);
+		textureTable.add(texture2).pad(100);
+		textureTable.add(texture3).pad(100);
+
 		table.add(backButton).bottom().right().expand().pad(70);
 
+		Image background = new Image(new Texture("tBackground.jpg"));
+		background.setSize(JumazyController.WORLD_WIDTH, JumazyController.WORLD_HEIGHT);
+		stage.addActor(background);
 		stage.addActor(table);
+		stage.addActor(textureTable);
 	}
 
 	@Override
