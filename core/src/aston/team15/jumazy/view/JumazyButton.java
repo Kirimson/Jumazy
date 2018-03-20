09eb8a5df94e9c5e38a351124dcf7917c288ba5b
Kirimson <kierangates@outlook.com>
2018-03-20 01:16:14 +0000

    butt sound

--- core/src/aston/team15/jumazy/view/JumazyButton.java
+++ core/src/aston/team15/jumazy/view/JumazyButton.java
@@ -1,5 +1,6 @@
 package aston.team15.jumazy.view;
 
+import aston.team15.jumazy.controller.GameSound;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -7,13 +8,17 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class JumazyButton extends TextButton {
 
     public JumazyButton(String text, Skin skin){
         super(text, skin);
 
-        this.addListener(new InputListener() {
+        this.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                GameSound.playButtonSound();
+            }
             public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
                 Pixmap cursor = new Pixmap(Gdx.files.internal("hand.png"));
                 Gdx.graphics.setCursor(Gdx.graphics.newCursor(cursor, 0, 0));
